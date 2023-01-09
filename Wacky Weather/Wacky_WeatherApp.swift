cb002c63e6c2f968c626c3807751b9515b8b5afc
oniszczak <aleks42@gmail.com>
2023-01-09 10:49:43 -0500

    Initial Commit

---
+++ Wacky Weather/Wacky_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wacky_WeatherApp.swift
+//  Wacky Weather
+//
+//  Created by ALEKSANDER ONISZCZAK on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct Wacky_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
