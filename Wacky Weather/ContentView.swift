4e48a1d41c18244f500c3464c5669f69f9efc037
oniszczak <aleks42@gmail.com>
2023-01-09 19:56:24 -0500

    df

--- Wacky Weather/ContentView.swift
+++ Wacky Weather/ContentView.swift
@@ -6,16 +6,54 @@
 //
 
 import SwiftUI
+import CoreLocation
+import WeatherKit
+
+//var weatherService = WeatherService ()
+//var syracuse = CLLocation (latitude: 43, longitude: -76)
+//var weather = try! await weatherService.weather(for: syracuse)
+//var temperature = weather.currentWeather.temperature
+//var uvIndex = weather.currentWeather.uvIndex
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    static let location =
+    CLLocation(
+        latitude: .init(floatLiteral: 43.6588820),
+        longitude: .init(floatLiteral: -79.4852880)
+    )
+    
+    @State var weather: Weather?
+    
+    func getWeather() async {
+        do {
+            weather = try await Task
+            {
+                try await WeatherService.shared.weather(for: Self.location)
+            }.value
+        } catch {
+            fatalError("\(error)")
         }
-        .padding()
+    }
+    
+    var body: some View {
+        
+        if let weather = weather {
+            VStack{
+                Text("Toronto").font(.largeTitle).padding().foregroundColor(.blue)
+                Text(weather.currentWeather.temperature.description)
+                    .font(.system(size: 60))
+                Text(weather.currentWeather.condition.description).font(.title)
+                Image(systemName: weather.currentWeather.symbolName).font(.title)
+                
+            }
+            } else {
+                    ProgressView()
+                    .task {
+                        await getWeather ()
+                    }
+            }
+        
     }
 }
 
---
+++ Wacky Weather/WeatherKitManager.swift
@@ -0,0 +1,41 @@
+//
+//  WeatherKitManager.swift
+//  Wacky Weather
+//
+//  Created by ALEKSANDER ONISZCZAK on 2023-01-09.
+//
+
+import Foundation
+import WeatherKit
+
+@MainActor class WeatherKitManager: ObservableObject {
+
+     @Published var weather: Weather?
+
+
+     func getWeather() async {
+     func getWeather(latitude: Double, longitude: Double) async {
+         do {
+             weather = try await Task.detached(priority: .userInitiated) {
+                 return try await WeatherService.shared.weather(for: .init(latitude: 37.322998, longitude: -122.032181))  // Coordinates for Apple Park just as example coordinates
+                 return try await WeatherService.shared.weather(for: .init(latitude: latitude, longitude: longitude))
+             }.value
+         } catch {
+             fatalError("\(error)")
+        }
+    }
+    
+    var symbol: String {
+        weather?.currentWeather.symbolName ?? "xmark"
+    }
+    
+    var temp: String {
+        let temp =
+        weather?.currentWeather.temperature
+        
+        let convert = temp?.converted(to: .fahrenheit).description
+        return convert ?? "Loading Weather Data"
+        
+    }
+    
+}
